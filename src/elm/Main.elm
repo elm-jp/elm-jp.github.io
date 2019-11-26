63ab4513d28e1cb034303bdc327f735a4c1770a4
y047aka <yoshitaka.totsuka@siiibo.com>
2019-11-27 01:25:28 +0900

    Change main from document to application

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,16 +1,26 @@
 module Main exposing (main)
 
 import Browser
+import Browser.Navigation as Nav
 import Page.Top
+import Route exposing (Route)
+import Url
+import Url.Parser as Parser
+
+
+
+-- MAIN
 
 
 main : Program () Model Msg
 main =
-    Browser.document
+    Browser.application
         { init = init
         , update = update
         , view = view
         , subscriptions = \_ -> Sub.none
+        , onUrlChange = UrlChanged
+        , onUrlRequest = UrlRequested
         }
 
 
@@ -19,15 +29,41 @@ main =
 
 
 type alias Model =
-    { userState : String
+    { key : Nav.Key
+    , page : Page
     }
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( Model ""
-    , Cmd.none
-    )
+type Page
+    = NotFound
+    | Top
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url key =
+    routing url
+        { key = key
+        , page = Top
+        }
+
+
+
+-- ROUTER
+
+
+routing : Url.Url -> Model -> ( Model, Cmd Msg )
+routing url model =
+    let
+        maybeRoute : Maybe Route
+        maybeRoute =
+            Parser.parse Route.parser url
+    in
+    case maybeRoute of
+        Nothing ->
+            ( { model | page = NotFound }, Cmd.none )
+
+        Just Route.Top ->
+            ( { model | page = Top }, Cmd.none )
 
 
 
@@ -35,14 +71,23 @@ init _ =
 
 
 type Msg
-    = NoOp
+    = UrlRequested Browser.UrlRequest
+    | UrlChanged Url.Url
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        UrlRequested urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            routing url model
 
 
 
@@ -51,6 +96,13 @@ update msg model =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = "Elm-jp"
-    , body = Page.Top.view
-    }
+    case model.page of
+        NotFound ->
+            { title = "Elm-jp"
+            , body = Page.Top.view
+            }
+
+        Top ->
+            { title = "Elm-jp"
+            , body = Page.Top.view
+            }
