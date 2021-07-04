dc0051e932a2b521fa4dc9ade9b53aecee6e5769
Yoshitaka Totsuka <y047aka@gmail.com>
2021-07-04 15:38:15 +0900

    update siteHeader with tailwind css

--- src/Static/View.elm
+++ src/Static/View.elm
@@ -1,19 +1,31 @@
 module Static.View exposing (siteFooter, siteHeader)
 
-import Html exposing (Html, a, div, footer, h1, header, nav, p, span, text)
-import Html.Attributes exposing (class, href)
+import Html exposing (Html, a, div, footer, h1, header, img, nav, p, span, text)
+import Html.Attributes exposing (class, href, rel, src, target)
 
 
 siteHeader : Html msg
 siteHeader =
-    header [ class "navbar is-spaced is-light" ]
-        [ h1 [ class "navbar-brand" ]
-            [ a [ class "navbar-item", href "/" ]
-                [ span [ class "has-text-weight-bold" ] [ text "Elm-jp" ]
+    header [ class "flex items-center justify-between flex-wrap bg-blue-500 p-3" ]
+        [ h1 [ class "flex text-white mr-6" ]
+            [ img [ class "fill-current h-8 w-8 mr-2", src "/images/elm-japan-icon.svg" ] []
+            , a [ href "/" ] [ span [ class "font-semibold text-xl" ] [ text "Elm-jp" ] ]
                 ]
+        , nav [ class "hidden flex-grow sm:flex items-center" ]
+            [ div [ class "text-sm flex-grow" ] <|
+                List.map
+                    (\item ->
+                        a [ class "inline-block text-blue-200 hover:text-white mr-4", target "_blank", rel "noopener", href item.url ]
+                            [ text item.label ]
+                    )
+                    [ { label = "Guide（邦訳版）", url = "https://guide.elm-lang.jp" }
+                    , { label = "Discord", url = "https://discordapp.com/invite/4j2MxCg" }
+                    , { label = "ScrapBox", url = "https://scrapbox.io/elm-jp" }
             ]
-        , nav [ class "navbar-menu navbar-end" ]
-            [ a [ class "navbar-item", href "#sakura" ] [ text "さくらちゃん日記" ]
+            , div []
+                [ a [ class "inline-block text-sm px-4 py-2 border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white", href "#sakura" ]
+                    [ text "さくらちゃん日記" ]
+                ]
             ]
         ]
 
